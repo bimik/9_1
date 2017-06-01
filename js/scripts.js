52bb3f4d42335be46e3a5811868344b1d7bdd104
bimik <bimik@o2.pl>
2017-06-01 20:30:20 +0200

    1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // scripts.js
     
 function getTriangleArea(a, h) {
-    if (a <= 0 || h<= 0) {
+    if (a <= 0 || h <= 0) {
         return "Nieprawidłowe dane";
     } else {
         return a * h / 2;
